02a41a776a32b7b4268d7dafda266a7869ef2cfb
kroos180805 <thoufiqh56@gmail.com>
2025-10-06 23:26:37 +0530

    Character Property Analysis: Read a single character. Report if it is a letter, a digit, whitespace, or another symbol. Also report its case if it is a letter.

---
+++ CharacterPropertyAnalysis.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CharacterPropertyAnalysis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        if(Character.isLetter(ch)){
+            if(Character.isUpperCase(ch)){
+                System.out.println("Uppercase Letter");
+            } else {
+                System.out.println("Lowercase Letter");
+            }
+        }
+        else if(Character.isDigit(ch)){
+            System.out.println("Digit");
+        }
+        else if(Character.isWhitespace(ch)){
+            System.out.println("Whitespace");
+        }
+        else{
+            System.out.println("Symbol");
+        }
+    }
+}
