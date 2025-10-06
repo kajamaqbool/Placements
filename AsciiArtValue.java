0a5145138677a490b95e7f101d3aed9084d4c2af
kroos180805 <thoufiqh56@gmail.com>
2025-10-06 23:06:21 +0530

    first commit

---
+++ AsciiArtValue.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class AsciiArtValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = 0;
+
+        while (true) {
+            String line = sc.nextLine();
+            if (line.equals("end")) {
+                break;
+            }
+            for(int i=0; i<line.length(); i++) {
+                total += (int) line.charAt(i);
+            }
+        }
+        System.out.println(total);
+        sc.close();
+    }
+}
