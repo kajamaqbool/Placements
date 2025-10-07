fa6105ebab10d9846ef7c5185f06066651e6341e
KAJA MAGBOOL <thoufiqh56@gmail.com>
2025-10-07 11:44:08 +0530

    Update CharacterPropertyAnalysis.java

--- CharacterPropertyAnalysis.java
+++ CharacterPropertyAnalysis.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CharacterPropertyAnalysis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char ch = sc.next().charAt(0);
+        char ch = sc.nextLine().charAt(0);
 
         if(Character.isLetter(ch)){
             if(Character.isUpperCase(ch)){
